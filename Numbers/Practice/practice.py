4cda56d74827d82b9febdaf73359e3622b2c4cdd
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2026-02-20 16:18:13 +0530

    practice on numbers

---
+++ Numbers/Practice/practice.py
@@ -0,0 +1,28 @@
+# Rounding Numbers
+
+import math
+
+price=35.54879665
+print(round(price))
+
+print(math.floor(price))
+
+
+
+# Random
+
+import random
+
+print(random.random())
+print(random.randint(1,6))
+
+
+# Validation
+x=7.0
+print(x.is_integer())
+
+x=4.1
+print(x.is_integer())
+
+y=7.0
+print(isinstance(y,int))
